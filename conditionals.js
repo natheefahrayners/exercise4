c34c82d78e2f1d93a2219c9e0a277ff9c79ff016
natheefahrayners <natheefahrayners4@gmail.com>
2025-10-31 10:07:31 +0200

    exercise_4

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+let score = 90;
+let passingGrade = 60;
+
+if (score === 100) {
+    // Code for a perfect score
+    console.log("Perfect score! Amazing work!");
+} else if (score >= passingGrade) {
+    // Code for a regular passing score
+    console.log("Congratulations, you passed!");
+} else {
+    // Code for a failing score
+    console.log("You did not pass. Keep trying!");
+}
+
+
+
+
